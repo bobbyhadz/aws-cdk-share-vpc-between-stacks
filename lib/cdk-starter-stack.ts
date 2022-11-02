8fd94065f101fd65f199e77b8f6c503348fe7933
Borislav Hadzhiev <opmftw@gmail.com>
2022-11-02 21:46:45 +0200

    update CDK version

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -44,7 +44,7 @@ export class LambdaStack extends cdk.Stack {
       // 👇 place lambda in shared VPC
       vpc,
       allowPublicSubnet: true,
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'index.main',
       code: lambda.Code.fromAsset(path.join(__dirname, '/../src/my-lambda')),
       environment: {
